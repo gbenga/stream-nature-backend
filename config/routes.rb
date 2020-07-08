5b3f3bc1a1b3d54950a9ff89aaddf772febf7345
gojoaromokudu <gojoaromokudu@gmail.com>
2020-07-08 12:00:47 +0100

    fetching data now works with rails backend

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      resources :users, only: [:index, :show]
+      resources :users, only: [:index, :show, :new, :create]
       resources :locations, only: [:index, :show]
       resources :events, only: [:index]
     end
