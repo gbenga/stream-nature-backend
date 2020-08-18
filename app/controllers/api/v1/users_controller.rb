1d9d7b39e5a352bf0be7d1e8bc55aa5a740dd0ca
gojoaromokudu <gojoaromokudu@gmail.com>
2020-08-18 18:48:31 +0100

    Update Auth

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::UsersController < ApplicationController
+    skip_before_action :authorised?, only: [:create, :sign_in]
 
     def index
         users = User.all 
@@ -30,30 +31,22 @@ class Api::V1::UsersController < ApplicationController
     end
 
     def sign_in
-        #Try an find a user with the username send in the params hash
         user = User.find_by(username: params[:username])
-        #If we can find them, authenticate the user with the password we've been given
+
         if user && user.authenticate(params[:password])
-         #If it was successful, log them in
-         render json: {username: user.username, token: generate_token({id: user.id})}
-         #Otherwise send back an authentication error
+         render json: {user: user, token: generate_token({id: user.id})}
         else 
-            render json: {message: "Cant find a user with that username OR incorrect password"}
+            render json: {errors: "You could not be signed in. Check your username and password combination"}
         end
     end
 
     def validate
-     #Decode the token that’s been sent as the authorization header & get the id of the logged in user
-     id = decode_token 
-     #Use that id to find the logged-in user 
-     user = User.find_by(id: id)
-     #Respond with the username of that user & generate a new token from their id
-     if user 
-        render json: {username: user.username, token: generate_token({id: user.id})}
-        #Otherwise send back an authentication error
-     else 
-        render json: {message: "Cant find a user with that id OR decode the JWT token"}
-     end
+        user = logged_in_user
+        if user 
+            render json: {user: user, token: generate_token({id: user.id})}
+        else 
+            render json: {message: "Failed to validate this user"}
+        end
     end
 
     private
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,6 @@
 class User < ApplicationRecord
-    # Auth
     has_secure_password
 
     has_many :events
     has_many :locations, through: :events
-   
-    # validates :username, uniqueness: { case_sensitive: false }
 end
